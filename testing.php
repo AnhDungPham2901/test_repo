6909a18eaf3478412b22d76a3f3c314e19d105df
AnhDungPham2901 <dungpham290198@gmail.com>
2020-12-20 23:24:03 +0700

    test zsh terminal

--- testing.php
+++ testing.php
@@ -5,3 +5,8 @@ echo "added new line from vscode";
 echo "add new line de tao conflict o local";
 echo "ahihi do ngok";
 echo "test git push bang gp";
+
+
+$a= 10;
+
+<!-- type cmd + shift + p ==> toggle zen mode to focus - not show gitlens extension -->
