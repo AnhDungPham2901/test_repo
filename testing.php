6c022b6e367d09dc1abaf46a0f69269884312adf
AnhDungPham2901 <dungpham290198@gmail.com>
2020-12-23 06:32:54 +0700

    test git again

--- testing.php
+++ testing.php
@@ -6,12 +6,4 @@ echo "add new line de tao conflict o local";
 echo "ahihi do ngok";
 echo "test git push bang gp";
 
-
-$a= 10;
-
-<!-- type cmd + shift + p ==> toggle zen mode to focus - not show gitlens extension -->
-echo "test pull with zsh";
-
-
-// ADDED new line
-//Test merge - default strategy
+// THIS IS FOR GIT 2
